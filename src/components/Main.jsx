ff66b4246320e26ef865cb6f82880fefe549a155
Jairo Alves <jairocreta@hotmail.com>
2019-09-23 06:03:56 -0300

    inclusao firebase

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import firebase from '../config/firebase'
 
 const Main = props => {
+
+    const [name,SetName] = useState('')
+    /* useEffect(()=>{
+       firebase.database().ref('usuarios').child('usuarios')
+    },[]) */
+
+    console.log(name)
+
+    const changeName = (e) => {
+        SetName(e.target.value)
+    }
+
+    const addDescrition = name => {
+        /* firebase.database().ref('usuarios').push({name})   
+        SetName('') */
+        firebase.post('/contacts.json', {name})
+    }
+
     return(
         <main className="main">
-            <h1>Principal</h1>
+            <input value={name} onChange={(e)=>changeName(e)}></input>
+            <button  onClick={()=>addDescrition(name)}>Enviar</button>
         </main>
     )
 }
